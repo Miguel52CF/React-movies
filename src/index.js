52a2b9c3e5a32467f0581464782ce8a0117f29b6
Miguel Angel <miguelangelcf52@gmail.com>
2022-10-19 10:42:35 -0500

    probando react0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Componente ({titulo,children}){
+  return (
+  <div>
+  <h1>{titulo}</h1>
+  <div>{children}</div>  
+</div> 
+); 
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Componente titulo="Titulo 1"> Hola desde react</Componente> 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
